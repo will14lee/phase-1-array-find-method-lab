aec9bad2cbb039911e472b2f8f31bbe94770b9a9
will14lee <will14lee@gmail.com>
2021-06-12 17:39:56 -0400

    test

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const superbowlWin=record.find(result=>{return result.result=== "W"})
+//superbowlWin;
+
+  it('returns undefined if the record has no win objects', function() {
+    const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+    expect(superbowlWin(sadReality)).to.equal(undefined)
 
